408b6da7146c5dd8eeddbb8fdfd4d39ec536c04d
Angelo Geulin <angelogeulin123@gmail.com>
2023-01-16 00:44:09 +0800

    feat: add endpoints for searching users by name

---
+++ server/api/users/archived/search/by-name/[nameFilter].ts
@@ -0,0 +1,37 @@
+import { ZodError, z } from "zod"
+import { getArchivedUsersByName } from "../../../../../../models/users"
+
+const InputSchema = z.object({
+  nameFilter: z.string(),
+})
+
+export default defineEventHandler(async (event) => {
+  const { db } = event.context.firebase
+  try {
+    const { nameFilter } = getRouterParams(event)
+    const input = InputSchema.parse({
+      nameFilter,
+    })
+    const users = await getArchivedUsersByName(db, input.nameFilter)
+
+    return {
+      message: "User profile retrieved",
+      payload: {
+        users,
+      },
+    }
+  } catch (e) {
+    if (e instanceof ZodError)
+      throw createError({
+        statusCode: 400,
+        statusMessage: "Bad Request",
+        message: `Validation error occured: ${e.message}`,
+      })
+
+    throw createError({
+      statusCode: 500,
+      statusMessage: "Unknown Error",
+      message: `Unknown error occured: ${e}`,
+    })
+  }
+})
