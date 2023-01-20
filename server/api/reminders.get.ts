fdbb71252bde2a49cdcbd498769d1b158618f942
Angelo Geulin <angelogeulin123@gmail.com>
2023-01-21 00:18:58 +0800

    feat: add route protection to reminders routes

--- server/api/reminders.get.ts
+++ server/api/reminders.get.ts
@@ -1,5 +1,22 @@
 export default defineEventHandler(async (event) => {
-  const { db } = event.context.firebase
+  const {
+    firebase: { db },
+    user,
+  } = event.context
+
+  if (!user)
+    throw createError({
+      statusCode: 401,
+      statusMessage: "Unauthorized",
+      message: "You must login to update your full name.",
+    })
+
+  if (user.accountType !== "admin")
+    throw createError({
+      statusCode: 401,
+      statusMessage: "Unauthorized",
+    })
+
   try {
     const docSnap = await db.collection("reminders").doc("reminders").get()
 
