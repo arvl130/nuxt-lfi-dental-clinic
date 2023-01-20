a07f4f0df3217cf520998aa2e1df455ef4fb3b02
Angelo Geulin <angelogeulin123@gmail.com>
2023-01-21 00:17:55 +0800

    feat: add route protection to message routes

--- server/api/messages.get.ts
+++ server/api/messages.get.ts
@@ -1,7 +1,24 @@
 import { getAllMessages } from "../../models/messages"
 
 export default defineEventHandler(async (event) => {
-  const { db } = event.context.firebase
+  const {
+    firebase: { db },
+    user,
+  } = event.context
+
+  if (!user)
+    throw createError({
+      statusCode: 401,
+      statusMessage: "Unauthorized",
+      message: "You must login to update your full name.",
+    })
+
+  if (user.accountType !== "admin")
+    throw createError({
+      statusCode: 401,
+      statusMessage: "Unauthorized",
+    })
+
   try {
     const messages = await getAllMessages(db)
 
--- server/api/messages/[uid]/delete.delete.ts
+++ server/api/messages/[uid]/delete.delete.ts
@@ -6,7 +6,24 @@ const InputSchema = z.object({
 })
 
 export default defineEventHandler(async (event) => {
-  const { db } = event.context.firebase
+  const {
+    firebase: { db },
+    user,
+  } = event.context
+
+  if (!user)
+    throw createError({
+      statusCode: 401,
+      statusMessage: "Unauthorized",
+      message: "You must login to update your full name.",
+    })
+
+  if (user.accountType !== "admin")
+    throw createError({
+      statusCode: 401,
+      statusMessage: "Unauthorized",
+    })
+
   try {
     const { uid } = getRouterParams(event)
 
